18302f17ffeb2ff9589a535e2953cf50ae84662a
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-03-29 12:09:34 +0200

    Move build script to travis and wercker configs.

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-composer self-update
-composer install --no-interaction --prefer-source --dev
-pear upgrade PEAR
-pear config-set auto_discover 1
-pear install pear.phpunit.de/PHPUnit
-vendor/bin/phpunit --coverage-clover=coverage.xml
-g++ web/gsqasm.cpp -o web/gsqasm
